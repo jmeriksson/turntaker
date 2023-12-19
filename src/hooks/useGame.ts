4e8c749ea658354d32b549b846f923ece266ce72
Mikael Eriksson <mikael.eriksson.nkpg@gmail.com>
2023-12-19 13:05:56 +0100

    adding missing prop

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -7,7 +7,8 @@ export default function useGame(): TGameContext {
 		return {
 			state: {
 				isPlaying: false,
-				players: []
+				players: [],
+				diceModalIsOpen: false,
 			},
 			dispatch: () => {}
 		}
